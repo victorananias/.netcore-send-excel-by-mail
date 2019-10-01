aca20edd6ba7c938a227033156bf72f3ef4b57d8
Victor <victor@ananias.dev>
2019-10-01 14:40:27 -0300

    configuration file working

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace ConsoleApp
 {
     class Program
     {
+        private static IConfiguration _configuration;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LoadConfiguration();
+            
+            Console.WriteLine("conn: " + _configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        private static void LoadConfiguration()
+        {
+            _configuration = (new ConfigurationBuilder())
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
         }
     }
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,6 @@
 # Base DotNetCore Console App
 
+## Packages
+* Microsoft.Extensions.Configuration
+* Microsoft.Extensions.Configuration.FileExtensions
+* Microsoft.Extensions.Configuration.Json
