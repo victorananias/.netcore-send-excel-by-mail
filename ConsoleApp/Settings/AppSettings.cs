d77a0d05a390a9d50b0ffd04abfbcdceebd145ba
Victor <victor@ananias.dev>
2020-01-25 09:35:16 -0300

    AppSettings removed

--- ConsoleApp/Settings/AppSettings.cs
+++ ConsoleApp/Settings/AppSettings.cs
@@ -4,23 +4,7 @@ using System.Text;
 
 namespace ConsoleApp.Settings
 {
-    class ConnectionStrings
-    {
-        public string DefaultConnection { get; set; }
-    }
-
-    class Mail
-    {
-        public string Host { get; set; }
-        public int Port { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-
     class AppSettings
     {
-        public ConnectionStrings ConnectionStrings { get; set; }
-        public Mail Mail { get; set; }
-
     }
 }
